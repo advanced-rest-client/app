e0b9bab249f0522a6a4000a6e4fbe7d0ee0c5bc6
Pawel Psztyc <jarrodek@gmail.com>
2021-11-09 17:26:40 -0800

    chore: making sure all libraries are exported
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -5,33 +5,38 @@ export { RenderableMixin } from './src/mixins/RenderableMixin';
 export { BaseThemeManager } from './src/lib/themes/BaseThemeManager';
 export * as Route from './src/lib/route';
 
-export { ApplicationScreen } from './src/pages/ApplicationScreen';
+// app screens
 export { AboutScreen } from './src/pages/AboutScreen';
-export { MenuScreen } from './src/pages/MenuScreen';
-export { SearchBar } from './src/pages/SearchBar';
-export { ThemesScreen } from './src/pages/ThemesScreen';
-export { LicenseScreen } from './src/pages/LicenseScreen';
 export { AnalyticsConsentScreen } from './src/pages/AnalyticsConsentScreen';
+export { ApiConsoleScreen } from './src/pages/ApiConsoleScreen';
+export { ApplicationScreen } from './src/pages/ApplicationScreen'
 export { ArcScreen } from './src/pages/ArcScreen';
 export { ArcScreenWeb } from './src/pages/ArcScreenWeb';
-export { ApiConsoleScreen } from './src/pages/ApiConsoleScreen';
 export { DataImportScreen } from './src/pages/DataImportScreen';
 export { DataImportScreenWeb } from './src/pages/DataImportScreenWeb';
 export { GoogleDrivePickerScreen } from './src/pages/GoogleDrivePickerScreen';
+export { LicenseScreen } from './src/pages/LicenseScreen';
+export { MenuScreen } from './src/pages/MenuScreen';
+export { SearchBar } from './src/pages/SearchBar';
+export { ThemesScreen } from './src/pages/ThemesScreen';
+
+// elements
+export { default as ApiEntrypointSelectorElement } from './src/elements/application/ApiEntrypointSelectorElement';
+export { default as ArcApplicationMenuElement } from './src/elements/application/ArcApplicationMenuElement';
 
 // bindings
-export { PlatformBindings } from './src/bindings/base/PlatformBindings';
+export { ApiParserBindings } from './src/bindings/base/ApiParserBindings';
 export { ApplicationBindings } from './src/bindings/base/ApplicationBindings';
 export { ConfigurationBindings } from './src/bindings/base/ConfigurationBindings';
-export { ThemeBindings } from './src/bindings/base/ThemeBindings';
-export { WorkspaceBindings } from './src/bindings/base/WorkspaceBindings';
 export { DataExportBindings } from './src/bindings/base/DataExportBindings';
 export { EncryptionBindings } from './src/bindings/base/EncryptionBindings';
 export { GoogleDriveBindings } from './src/bindings/base/GoogleDriveBindings';
-export { OAuth2Bindings } from './src/bindings/base/OAuth2Bindings';
-export { ApiParserBindings } from './src/bindings/base/ApiParserBindings';
 export { HttpRequestBindings } from './src/bindings/base/HttpRequestBindings';
 export { NavigationBindings } from './src/bindings/base/NavigationBindings';
+export { OAuth2Bindings } from './src/bindings/base/OAuth2Bindings';
+export { PlatformBindings } from './src/bindings/base/PlatformBindings';
+export { ThemeBindings } from './src/bindings/base/ThemeBindings';
+export { WorkspaceBindings } from './src/bindings/base/WorkspaceBindings';
 
 // shortcuts
 export { MonacoLoader } from "@advanced-rest-client/monaco-support";
--- index.js
+++ index.js
@@ -5,33 +5,38 @@ export { RenderableMixin } from './src/mixins/RenderableMixin.js';
 export { BaseThemeManager } from './src/lib/themes/BaseThemeManager.js';
 export * as Route from './src/lib/route.js';
 
-export { ApplicationScreen } from './src/pages/ApplicationScreen.js'
+// app screens
 export { AboutScreen } from './src/pages/AboutScreen.js';
-export { MenuScreen } from './src/pages/MenuScreen.js';
-export { SearchBar } from './src/pages/SearchBar.js';
-export { ThemesScreen } from './src/pages/ThemesScreen.js';
-export { LicenseScreen } from './src/pages/LicenseScreen.js';
 export { AnalyticsConsentScreen } from './src/pages/AnalyticsConsentScreen.js';
+export { ApiConsoleScreen } from './src/pages/ApiConsoleScreen.js';
+export { ApplicationScreen } from './src/pages/ApplicationScreen.js'
 export { ArcScreen } from './src/pages/ArcScreen.js';
 export { ArcScreenWeb } from './src/pages/ArcScreenWeb.js';
-export { ApiConsoleScreen } from './src/pages/ApiConsoleScreen.js';
 export { DataImportScreen } from './src/pages/DataImportScreen.js';
 export { DataImportScreenWeb } from './src/pages/DataImportScreenWeb.js';
 export { GoogleDrivePickerScreen } from './src/pages/GoogleDrivePickerScreen.js';
+export { LicenseScreen } from './src/pages/LicenseScreen.js';
+export { MenuScreen } from './src/pages/MenuScreen.js';
+export { SearchBar } from './src/pages/SearchBar.js';
+export { ThemesScreen } from './src/pages/ThemesScreen.js';
+
+// elements
+export { default as ApiEntrypointSelectorElement } from './src/elements/application/ApiEntrypointSelectorElement.js';
+export { default as ArcApplicationMenuElement } from './src/elements/application/ArcApplicationMenuElement.js';
 
 // bindings
-export { PlatformBindings } from './src/bindings/base/PlatformBindings.js';
+export { ApiParserBindings } from './src/bindings/base/ApiParserBindings.js';
 export { ApplicationBindings } from './src/bindings/base/ApplicationBindings.js';
 export { ConfigurationBindings } from './src/bindings/base/ConfigurationBindings.js';
-export { ThemeBindings } from './src/bindings/base/ThemeBindings.js';
-export { WorkspaceBindings } from './src/bindings/base/WorkspaceBindings.js';
 export { DataExportBindings } from './src/bindings/base/DataExportBindings.js';
 export { EncryptionBindings } from './src/bindings/base/EncryptionBindings.js';
 export { GoogleDriveBindings } from './src/bindings/base/GoogleDriveBindings.js';
-export { OAuth2Bindings } from './src/bindings/base/OAuth2Bindings.js';
-export { ApiParserBindings } from './src/bindings/base/ApiParserBindings.js';
 export { HttpRequestBindings } from './src/bindings/base/HttpRequestBindings.js';
 export { NavigationBindings } from './src/bindings/base/NavigationBindings.js';
+export { OAuth2Bindings } from './src/bindings/base/OAuth2Bindings.js';
+export { PlatformBindings } from './src/bindings/base/PlatformBindings.js';
+export { ThemeBindings } from './src/bindings/base/ThemeBindings.js';
+export { WorkspaceBindings } from './src/bindings/base/WorkspaceBindings.js';
 
 // shortcuts
 export { MonacoLoader } from "@advanced-rest-client/monaco-support";
